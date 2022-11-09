2b5d194948e100df39fba0ce7722122702247529
jiongxie <byeonhs0018@naver.com>
2022-11-09 15:35:24 +0900

    array

---
+++ array.js
@@ -0,0 +1,19 @@
+// array
+const array1 = [1,2,3,4,5,6,7,8];
+console.log(array1);
+
+let array2 = new Array(8);
+console.log(array2);
+
+array = Array.from({
+	0 : 'h',
+	1 : 'e',
+	2 : 'l',
+	3 : 'l',
+	4 : 'o',
+	5 : 'Y',
+	6 : 'O',
+	7 : '!',
+	length: 8,
+});
+console.log(array);
