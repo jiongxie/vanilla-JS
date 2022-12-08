3da81a5a5a13e05b2c7be88b5d28994075c03558
jiongxie <byeonhs0018@naver.com>
2022-12-08 21:04:34 +0900

    flatMap, sort, reduce

--- es6/function.js
+++ es6/function.js
@@ -55,3 +55,32 @@ console.log(found5);
 const found6 = lng.filter((value) => value.eng === 'a');
 
 console.log(found6);
+
+// flatMap
+const noms = [1,2,3,4,5,6,7,8];
+
+nomsmap = noms.map((item) => ['a']);
+console.log(nomsmap);
+
+nomsflatmap = noms.flatMap((item) => ['a']);
+console.log(nomsflatmap);
+
+result = ['brian','jiongxie'].map((text) => text.split(''));
+console.log(result);
+
+result = ['brian','jiongxie'].flatMap((text) => text.split(''));
+console.log(result);
+
+
+// sort
+const sortnoms = [2,4,10,5,1,21];
+//sortnoms.sort();
+sortnoms.sort((a,b) => a-b); 
+console.log(sortnoms);
+
+// reduce
+result = [1,2,3,4,5,6,7,8,9,10].reduce((sum, value) => (sum += value) , 0);
+
+console.log(result);
+
+
