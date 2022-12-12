764767c4367cea2ee55ffa0d6f2d02b389a6a625
jiongxie <byeonhs0018@naver.com>
2022-12-12 19:00:04 +0900

    try catch

--- es6/async/callback.js
+++ es6/async/callback.js
@@ -9,3 +9,22 @@ function clbk() {
 
 }
 clbk();
+
+// try catch
+
+function Delay(callback, seconds) {
+	if(!callback) {
+		throw new Error('callback function must be');
+	}
+	if(!seconds || seconds < 0) {
+		throw new Error('seconds must over 0');
+	}
+	setTimeout(callback, seconds * 1000);
+}
+
+
+try {
+	Delay(() => {console.log('timer on');},-1); 
+} catch (error) {
+	console.log('unstatisfied requirment');
+}
